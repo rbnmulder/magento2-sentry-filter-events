8d91bb6d84daf1259e8fafa8e6fb17ba9ced85d9
Robin Mulder <robin@justbetter.nl>
2023-06-30 13:29:56 +0200

    Process feedback. Make it possible to filter messages without exception

--- Observer/BeforeSending.php
+++ Observer/BeforeSending.php
@@ -30,19 +30,19 @@ class BeforeSending implements ObserverInterface
         $event = $observer->getEvent()->getSentryEvent()->getEvent();
         $hint = $observer->getEvent()->getSentryEvent()->getHint();
 
-        if ($hint?->exception !== null) {
-            $hintMessage = $hint->exception->getMessage();
-            if ($hint->exception instanceof LocalizedException) {
-                $hintMessage = $hint->exception->getRawMessage();
-                $event->setMessage($hintMessage);
-            }
+        $hintMessage = $hint?->exception?->getMessage() ?? $event->getMessage();
+        if ($hint?->exception instanceof LocalizedException) {
+            $hintMessage = $hint->exception->getRawMessage();
+            $event->setMessage($hintMessage);
+        }
 
-            $messages = $this->json->unserialize($this->scopeConfig->getValue('sentry/event_filtering/messages'));
-            foreach ($messages as $message) {
-                if (str_contains($hintMessage, $message['message'])) {
-                    $observer->getEvent()->getSentryEvent()->unsEvent();
-                }
+        $messages = $this->json->unserialize($this->scopeConfig->getValue('sentry/event_filtering/messages'));
+        foreach ($messages as $message) {
+            if (str_contains($hintMessage, $message['message'])) {
+                $observer->getEvent()->getSentryEvent()->unsEvent();
+                break;
             }
         }
+
     }
 }
